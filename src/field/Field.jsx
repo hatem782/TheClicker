258a19df24cf7aedc9627cb26633c26b2993c1c8
Hatem Ben Echikh <hatembenechikh100@gmail.com>
2024-08-09 18:37:11 +0100

    is mobile done

--- src/field/Field.jsx
+++ src/field/Field.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./field.css";
+import useIsMobile from "../hooks/useIsMobile";
 
 function Field({ socket }) {
   const [player, setPlayer] = useState(-1);
   const [position, setPosition] = useState(0);
   const [canPlay, setCanPlay] = useState(false);
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     socket.on("player", (resp) => {
@@ -61,6 +63,32 @@ function Field({ socket }) {
     };
   }, []);
 
+  return isMobile ? (
+    <VerticalField
+      player={player}
+      position={position}
+      Player1Click={Player1Click}
+      Player2Click={Player2Click}
+      Reset={Reset}
+    />
+  ) : (
+    <HorizentalField
+      player={player}
+      position={position}
+      Player1Click={Player1Click}
+      Player2Click={Player2Click}
+      Reset={Reset}
+    />
+  );
+}
+
+const HorizentalField = ({
+  player,
+  position,
+  Player1Click,
+  Player2Click,
+  Reset,
+}) => {
   return (
     <div className="field">
       <div
@@ -93,6 +121,47 @@ function Field({ socket }) {
       </div>
     </div>
   );
-}
+};
+
+const VerticalField = ({
+  player,
+  position,
+  Player1Click,
+  Player2Click,
+  Reset,
+}) => {
+  return (
+    <div className="field-vert">
+      <div
+        className="player1-vert"
+        style={{ height: `${100 + position}%` }}
+        onClick={Player1Click}
+      >
+        <span>
+          {player === 1 ? "ME" : "P1"} {100 + position}
+        </span>
+      </div>
+      <div
+        className="player2-vert"
+        style={{ height: `${100 - position}%` }}
+        onClick={Player2Click}
+      >
+        <span>
+          {player === 2 ? "ME" : "P2"} {100 - position}
+        </span>
+      </div>
+
+      <div
+        style={{
+          bottom: `${50 - position / 2}%`,
+        }}
+        className="score-vert"
+        onClick={Reset}
+      >
+        <span className="reset-vert">RESET</span>
+      </div>
+    </div>
+  );
+};
 
 export default Field;
